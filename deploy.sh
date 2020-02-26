e6201a18e1e727c93b73472ad34d1ad2d0da1092
zhangguo <zhangguo@inke.cn>
2020-02-26 10:24:45 +0800

    改为bash发布脚本

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,25 @@
 #!/usr/bin/env sh
 
-# 确保脚本抛出遇到的错误
+# 当发生错误时中止脚本
 set -e
 
-# 发布
-npm run deploy
+# 构建
+npm run build
+
+# cd 到构建输出的目录下 
+cd dist
+
+# 部署到自定义域域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 部署到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 部署到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:fastCreator/vue-easy-admin-doc.git master:gh-pages
+
+cd -
